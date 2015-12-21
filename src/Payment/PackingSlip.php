810ddd40105b9e570de47695d74915f6fe3ef423
jimenator <jimenez.roque.d@gmail.com>
2015-12-21 20:41:21 +0100

    final por el momento

---
+++ src/Payment/PackingSlip.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jimenator
+ * Date: 21/12/15
+ * Time: 20:13
+ */
+
+namespace Payment;
+
+
+class PackingSlip
+{
+
+}
---
+++ src/Payment/UpgradeMembership.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jimenator
+ * Date: 21/12/15
+ * Time: 20:21
+ */
+
+namespace Payment;
+
+
+class UpgradeMembership
+{
+
+}
